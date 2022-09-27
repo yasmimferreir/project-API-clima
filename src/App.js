25ac2eb745b2776d2c3f393593ba32e8a14df808
Yasmim Ferreira Costa <yasmimferreiracost@gmail.com>
2022-09-27 19:36:36 -0400

    project-api-clima

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //variável de guardar valor = city
+  // setCity guarda o primeiro parâmetro = city
+
+  const [city, setCity] = useState("");
+  const [weatherForecast, setweatherForecast] = useState(null);
+
+  //toda vez que o usuario digitar alguma coisa, será redenrizado essa função
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  //
+  const handleSearch = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=22c54fd0267f8ac797057389aee685ed&lang=pt`
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setweatherForecast(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white" href="#top">
+          Previsão do tempo
         </a>
-      </header>
+      </nav>
+
+      <main className="container">
+        <div className="jumbotron">
+          <h1> Verifique a previsão do tempo da sua cidade em tempo real</h1>
+
+          <p className="lead">Digite o nome da sua cidade abaixo:</p>
+
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input
+                onChange={handleChange}
+                className="form-control"
+                value={city}
+              />
+            </div>
+          </div>
+
+          <button onClick={handleSearch} className="btn btn-primary btn-lg">
+            Pesquisar
+          </button>
+
+          {weatherForecast ? (
+            <div className="mt-5">
+              <div>
+                <p>País: {weatherForecast.sys.country}</p>
+                <p>Temperatura: {weatherForecast.main.temp}</p>
+                <p>Umidade: {weatherForecast.main.humidity}</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </main>
     </div>
   );
 }
